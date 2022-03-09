9e6bc39879019f621f4a1d171754c604a8e3d414
Roberto <maisto_roberto@hotmail.it>
2022-03-09 16:54:33 +0100

    WIP pass the first test in create_spec.rb still not passing in second test

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -5,6 +5,13 @@ class Api::CarsController < ApplicationController
   end
 
   def create
-    binding.pry
+    new_car = Car.new
+    new_car.brand = params[:car][:brand]
+    new_car.model = params[:car][:model]
+    new_car.colour = params[:car][:colour]
+    new_car.year = params[:car][:year]
+    new_car.price = params[:car][:price]
+    new_car.save
+    render json: { car: new_car }, status: 201
   end
 end
--- spec/requests/api/cars/create_spec.rb
+++ spec/requests/api/cars/create_spec.rb
@@ -1,16 +1,13 @@
 RSpec.describe 'POST /api/cars', type: :request do
-  # let!(:car) { create(:car, brand: 'Maserati') }
-  # let!(:car2) { create(:car, brand: 'Ferrari') }
-
   before do
     post '/api/cars', params: {
       car: { brand: 'Maserati', model: 'Levante Hybrid', colour: 'grey', year: 2021, price: 84.000 }
     }
   end
-  
+
   subject { response }
-  
-  it {is_expected.to have_http_status 201}
+
+  it { is_expected.to have_http_status 201 }
 
   it 'is exptected to return success message' do
     expect(response_json['message']).to eq 'The car was successfully added to your shopping cart'
